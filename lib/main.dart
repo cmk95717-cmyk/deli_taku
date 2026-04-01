feda100c829ccabdb56d70589d3ab5088d2a0eab
Chihiro <cmk.95717@gmail.com>
2026-04-02 07:18:47 +0900

    アプリアイコン、アプリ名変更

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliCalc',
+      title: 'デリ卓',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         useMaterial3: true,
